d7199eb20710b0792166a2ca471bdede7378f55f
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-15 21:09:24 -0300

    carrinho de compras realmente criado

--- typescript/carrinho.ts
+++ typescript/carrinho.ts
@@ -1,8 +1,14 @@
-interface Ccompras {
+interface produtos {
 
     descricao:string
     preco: number
     quantidade:number
 
 
+}
+var Ccompras: produtos = {
+    descricao:'',
+    quantidade:0,
+    preco:0
+    
 }
